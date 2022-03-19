56f6045402377ad455624823e30941894a75adaa
Mathieu <mathieu.toulon@free.fr>
2022-03-19 02:06:15 +0100

    Final style patch

--- pc-web-front/src/components/Footer.vue
+++ pc-web-front/src/components/Footer.vue
@@ -1,28 +1,29 @@
 <template>
   <v-footer color="primary lighten-1" padless>
-    <v-row justify="center" no-gutters>
-    
-      <v-btn color="white" text rounded>
-        <RouterLink to="/" class="footer-link">Home</RouterLink>
-      </v-btn>
-      <v-btn color="white" text rounded>
-        <RouterLink to="/apropos" class="footer-link">À propos</RouterLink>
-      </v-btn>
-      <v-btn color="white" text rounded>
-        <RouterLink to="/contact" class="footer-link">Contact</RouterLink>
-      </v-btn>
-      <v-btn color="white" text rounded>
-        <RouterLink to="/mentions" class="footer-link">Mentions légales</RouterLink>
-      </v-btn>
-      
-      <br />
-      
-      <v-col cols="12">
+
+    <v-row>
+	    <v-col> <v-btn color="white" text rounded>
+        	<RouterLink to="/" class="footer-link">Home</RouterLink>
+      	</v-btn></v-col>
+    	<v-col><v-btn color="white" text rounded>
+        	<RouterLink to="/apropos" class="footer-link">À propos</RouterLink>
+      	</v-btn></v-col>
+      	<v-col><v-btn color="white" text rounded>
+        	<RouterLink to="/contact" class="footer-link">Contact</RouterLink>
+      	</v-btn></v-col>
+      	<v-col><v-btn color="white" text rounded>
+        	<RouterLink to="/mentions" class="footer-link">Mentions légales</RouterLink>
+      	</v-btn></v-col>
+    </v-row>
+
+
+    <v-row>
+      <v-col>
         &copy; {{ new Date().getFullYear() }} —
         <RouterLink to="/" class="footer-link">Projet et Cohésion en Informatique</RouterLink>
       </v-col>
-      
     </v-row>
+
   </v-footer>
 </template>
 
@@ -41,6 +42,11 @@ footer {
 }
 
 v-btn {
-  margin-right: 2em;
+  margin: 2em;
+  text-align: center;
+}
+
+v-row {
+	display: block;
 }
 </style>
