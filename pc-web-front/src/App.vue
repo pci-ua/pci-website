4311057708603a16519a5e761e06a1cb95eae2e6
Mathieu <mathieu.toulon@free.fr>
2022-04-23 13:54:46 +0200

    adapt to respect linter syntax

--- pc-web-front/src/App.vue
+++ pc-web-front/src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import NavBar from '@/components/navigation/NavBar.vue'
 import PathNavigator from '@/components/navigation/PathNavigator.vue'
-import Footer from '@/components/Footer.vue'
+import Footer from '@/components/GenericFooter.vue'
 
 </script>
 
